270248931591d3b1d7b3ea73d9ec46a0aec6956b
TareqImam <tareqimam800@gmail.com>
2025-05-08 11:24:29 +0600

    chore: added conditional logic based on media type from nasa api

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
 export default function Home(props) {
-  const {data} = props;
+    const { data } = props;
+    console.log(data.media_type);
 
     return (
         <div className="image-container">
-            {/* <img src="/public/mars.png" alt="mars" className="bg-image" /> */}
-            <iframe 
-                src={`${data?.url}&autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&iv_load_policy=3&fs=0&disablekb=1&playsinline=1&loop=1`}
-                title={data?.title || 'mars'} 
-                className="bg-image"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                frameBorder="0"
-            />
+            {data.media_type === 'image' ? (
+                <img src={data.url} alt="mars" className="bg-image" />
+            ) : (
+                <iframe
+                    src={`${data?.url}&autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&iv_load_policy=3&fs=0&disablekb=1&playsinline=1&loop=1`}
+                    title={data?.title || 'mars'}
+                    className="bg-image"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    frameBorder="0"
+                />
+            )}
         </div>
     );
 }
